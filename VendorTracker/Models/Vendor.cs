eaabe29dfd22e389c445a2d3dd5154ce738a3bff
Ebru Rice <ebruerenb@gmail.com>
2022-02-26 23:27:40 -0800

    Add vendor and order classes and test order constructor

--- VendorTracker/Models/Vendor.cs
+++ VendorTracker/Models/Vendor.cs
@@ -4,6 +4,8 @@ namespace VendorTracker.Models
 {
   public class Vendor
   {
-    
+    public string name { get; set; }
+    public string description { get; set; }
+    private static List<Order> _instances = new List<Order> {};
   }
 }  
